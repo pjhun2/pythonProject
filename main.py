af775ff1f773a6984efeddaeb319976888cf9b64
pjhun2 <netwo115@naver.com>
2021-11-29 13:03:26 +0900

    Changes

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-price = 5000
-price *= 0.85
-print(price)
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
